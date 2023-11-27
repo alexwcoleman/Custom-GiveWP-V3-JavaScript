7994fbad8b0bfaf7ada869732d92245c2f64866f
alexwcoleman <alexanderwinstoncoleman@gmail.com>
2023-11-27 10:18:19 -0800

    V3!

--- assets/js/test-3958.js
+++
@@ -1,1 +0,0 @@
-console.log('Form 3958');
--- custom-givewp-js.php
+++ custom-givewp-js.php
@@ -1,25 +1,16 @@
 <?php
 /**
- * Plugin Name:		Custom GiveWP JS.
- * Description:		Custom JS inserted in GiveWP's iframe.
+ * Plugin Name:		Custom GiveWP V3 JS.
+ * Description:		Custom JS inserted in GiveWP's iframe for Visual Form Builder forms.
  * Author:			Alex Coleman
  * Version: 		1.0	
  */
 
 defined('ABSPATH') || exit;
 
-function add_custom_give_script() {
-	wp_enqueue_script( 'give-custom-script', plugin_dir_url( __FILE__ ) . 'assets/js/give-custom-script.js' );
+
+
+function givewp_custom_v3_script() {
+	wp_enqueue_script( 'give-custom-V3-script', plugin_dir_url( __FILE__ ) . 'assets/js/give-custom-V3-script.js', array(), '1.0.0', true );
 }
-add_action( 'give_embed_head', 'add_custom_give_script' );
-
-// ADDING SCRIPTS PER FORM
-function add_script_per_form( $form_id ) {
-
-	// Customize form title here or remove conditional for all forms.
-	if ( $form_id == 3958 ) {
-		wp_enqueue_script( 'give-3958', plugin_dir_url( __FILE__ ) . 'assets/js/test-3958.js' );
-	}
-}
-
-add_action( 'give_pre_form_output', 'add_script_per_form', 10, 1 );
+add_action( 'givewp_donation_form_enqueue_scripts', 'givewp_custom_v3_script' );
